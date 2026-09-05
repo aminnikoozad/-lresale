18b282733769b347d351ec600b1d75787bb366de
aminnikoozad <aminnikoozad1371@gmail.com>
2026-09-05 17:27:33 -0400

    Make account loading skeleton responsive

--- app/account/loading.tsx
+++ app/account/loading.tsx
@@ -5,7 +5,7 @@ export default function AccountLoading() {
       <div style={{ maxWidth: 1240, margin: "42px auto 0" }}>
         <div className="skeleton-line" style={{ width: 150, marginBottom: 14 }} />
         <div className="skeleton-box" style={{ width: "min(620px,100%)", height: 64, marginBottom: 28 }} />
-        <div className="skeleton-grid" style={{ gridTemplateColumns: "repeat(3,minmax(0,1fr))", margin: 0 }}>
+        <div className="skeleton-grid skeleton-stats">
           {Array.from({ length: 3 }).map((_, index) => <div className="skeleton-box" style={{ height: 150 }} key={index} />)}
         </div>
         <div className="skeleton-box" style={{ height: 360, marginTop: 22 }} />
