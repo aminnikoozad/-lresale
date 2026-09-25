5a7131277235934ddc5daeac2f15e6d96a58db63
aminnikoozad <aminnikoozad1371@gmail.com>
2026-09-25 00:52:22 -0400

    Test strong password requirements

---
+++ tests/password-policy.test.ts
@@ -0,0 +1,19 @@
+import test from "node:test";
+import assert from "node:assert/strict";
+import { passwordPolicyError } from "../lib/password-policy.ts";
+
+test("requires uppercase, lowercase, number and symbol with at least eight characters", () => {
+  for (const weak of [
+    "short1!A",
+    "alllower1!",
+    "ALLUPPER1!",
+    "NoNumber!",
+    "NoSymbol1",
+    "Has Space1 ",
+  ]) {
+    assert.equal(typeof passwordPolicyError(weak), "string", weak);
+  }
+
+  assert.equal(passwordPolicyError("Strong1!"), null);
+  assert.equal(passwordPolicyError("Another?9Good"), null);
+});
